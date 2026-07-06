f65fc8de1f41131d43f026b0f33eaf754722d14e
ageeema <213107171+ageeema@users.noreply.github.com>
2026-07-06 23:53:33 +0530

    Added Day 10 solution

---
+++ Day10.java
@@ -0,0 +1,37 @@
+/*Problem: Read a string and check if it is a palindrome using two-pointer comparison.
+Input:
+- Single line: string s
+Output:
+- Print YES if palindrome, otherwise NO
+Example:
+Input:
+level
+Output:
+YES
+Explanation: String reads same forwards and backwards*/
+
+import java.util.Scanner;
+
+public class Day10 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                System.out.println("NO");
+                return;
+            }
+
+            left++;
+            right--;
+        }
+
+        System.out.println("YES");
+    }
+}
